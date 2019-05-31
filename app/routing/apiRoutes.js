c34ed50efc10a659997ea7aa3e69d96b1f49e75e
Lorenzo <lgdandrea4@gmail.com>
2019-05-31 15:50:20 -0500

    my paths are working correctly it is now down to getting the specific pieces of the JSON sending back in my get request to render to the DOM

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,18 +5,17 @@ var friends = require('../data/friends')
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json());
 
-
 api.post('/api', function(req, res){
     var newFriend = req.body;
     friends.push(newFriend);
     res.json(newFriend);
     console.log(friends);
-});
+})
 
 console.log(friends);
 
 api.get('/api/newfriend', function(req, res){
-    
+    return res.json(friends);
 })
 
 module.exports = api;
